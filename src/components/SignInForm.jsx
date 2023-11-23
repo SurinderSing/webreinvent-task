fc876cac01fa831b4b7cd1248ca6fe5a5a65512d
Surinder Singh <ssurindersingh100@gmail.com>
2023-11-23 15:26:27 +0530

    changes

---
+++ src/components/SignInForm.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import api from "../api/api";
+
+export default function SignInForm({ dispatch, navigate }) {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch({
+        type: "global/setLoading",
+        payload: true,
+      });
+      const { token } = await api.signIn(formData);
+      dispatch({
+        type: "global/setLoading",
+        payload: false,
+      });
+      if (token) {
+        setFormData({
+          email: "",
+          password: "",
+        });
+        dispatch({
+          type: "auth/login",
+          payload: {
+            token,
+          },
+        });
+        navigate("/dashboard");
+        return;
+      }
+      alert("failed to Signin");
+      return;
+    } catch (error) {
+      console.log(error);
+      alert("failed to Signin");
+    }
+    return;
+  };
+
+  return (
+    <div className="max-w-md mx-auto mt-8 p-8 border rounded-md shadow-lg bg-white">
+      <h2 className="text-2xl font-semibold mb-6">Sign In</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mt-4">
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-600"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            className="mt-1 p-2 w-full border rounded-md"
+            required
+          />
+        </div>
+
+        <div className="mt-4">
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-600"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            className="mt-1 p-2 w-full border rounded-md"
+            required
+          />
+        </div>
+
+        <div className="mt-6">
+          <button
+            type="submit"
+            className="bg-blue-500 text-white p-3 rounded-md w-full hover:bg-blue-600"
+          >
+            Sign In
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import api from "../api/api";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import SignInForm from "../components/SignInForm";
 
 const SignInPage = () => {
   const { isAuthenticated } = useSelector((state) => ({
@@ -16,104 +16,7 @@ const SignInPage = () => {
     }
   }, []);
 
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      dispatch({
-        type: "global/setLoading",
-        payload: true,
-      });
-      const { token } = await api.signIn(formData);
-      dispatch({
-        type: "global/setLoading",
-        payload: false,
-      });
-      if (token) {
-        setFormData({
-          email: "",
-          password: "",
-        });
-        dispatch({
-          type: "auth/login",
-          payload: {
-            token,
-          },
-        });
-        navigate("/dashboard");
-        return;
-      }
-      alert("failed to Signin");
-      return;
-    } catch (error) {
-      console.log(error);
-      alert("failed to Signin");
-    }
-    return;
-  };
-
-  return (
-    <div className="max-w-md mx-auto mt-8 p-8 border rounded-md shadow-lg bg-white">
-      <h2 className="text-2xl font-semibold mb-6">Sign In</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mt-4">
-          <label
-            htmlFor="email"
-            className="block text-sm font-medium text-gray-600"
-          >
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            className="mt-1 p-2 w-full border rounded-md"
-            required
-          />
-        </div>
-
-        <div className="mt-4">
-          <label
-            htmlFor="password"
-            className="block text-sm font-medium text-gray-600"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            className="mt-1 p-2 w-full border rounded-md"
-            required
-          />
-        </div>
-
-        <div className="mt-6">
-          <button
-            type="submit"
-            className="bg-blue-500 text-white p-3 rounded-md w-full hover:bg-blue-600"
-          >
-            Sign In
-          </button>
-        </div>
-      </form>
-    </div>
-  );
+  return <SignInForm dispatch={dispatch} navigate={navigate} />;
 };
 
 export default SignInPage;
